553b756c46f105d40960af73026d81694a059015
Annu Kumari <kumariannu972002@gmail.com>
2021-06-30 14:28:23 +0530

    commit

--- middleware/controllers/UserControllers.js
+++ middleware/controllers/UserControllers.js
@@ -62,23 +62,7 @@ exports.getUserById = (req, res) => {
      });
 };
 
-/* exports.findUserByFirstNameAndLastName = ((req, res) => {
-    let { firstName, lastName } = req.query;
-    User.find({ firstName: firstName, lastName: lastName })
-    .then((userArray) => {
-        if (userArray.length === 0) {
-            console.error(
-                `No user were found with first name: ${firstName} & last name:${lastName}`
-                );
-            return res.status(404).send("No user found");
-        }
-        return res.status(200).send(userArray);
-    })
-    .catch((error)  => {
-        console.error("Error occured", error);
-        return res.status(500).send("ERROR");
-    });
-}); */
+
 
 exports.updateUser = (req, res) => {
     let id = mongoose.Types.ObjectId(req.params.id);
